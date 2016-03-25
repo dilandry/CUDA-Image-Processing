e0f0958767942fa15b189d2117ae6a384e52c32c
Landry  Didier <landrydi@ug68.eecg>
2016-03-25 14:06:40 -0400

    Corrected compilation problem

--- cudaConvolution.h.cu
+++ cudaConvolution.h.cu
@@ -1,26 +1,4 @@
 #ifndef _CUDA_CONVOLUTION_H_CU_
 #define _CUDA_CONVOLUTION_H_CU_
 
-
-__global__ void   convolveBasic( 
-               int*   imgInPtr,    
-               int*   imgOutPtr,    
-               int    imgRows,    
-               int    imgCols,    
-               int*   imgPsfPtr,    
-               int    psfRowRad,
-               int    psfColRad);
-
-
-__global__ void   convolveBilateral( 
-               int*   devInPtr,    
-               int*   devOutPtr,    
-               int    imgRows,    
-               int    imgCols,    
-               int*   devPsfPtr,    
-               int    psfRowRad,
-               int    psfColRad,
-               int*   devPsfPtr,    
-               int    intensRad);
-
 #endif
